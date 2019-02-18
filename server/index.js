012d2094493db7d3a61f7a978674e305a71d4c5f
Austin <acreed1998@gmail.com>
2019-02-18 12:52:29 -0600

    add very basic framework for server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,11 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const _ = require('lodash');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('listening on port 3000!');
+});
