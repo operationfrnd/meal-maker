5fbe1c050522977806e3954354053bacc3918ff1
Austin <acreed1998@gmail.com>
2019-02-18 13:07:06 -0600

    server starts and works to an extent

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const axios = require('axios');
 const _ = require('lodash');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../index.html')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, '../index.html')));
+// app.use(express.static('../index.html'));
+
+app.get('/', (req, res) => {
+  console.log(req);
+  res.send("Done");
+})
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('listening on port 3000!');
+  console.log(`listening on port ${port}!`);
 });
