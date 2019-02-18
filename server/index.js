6d539133fdb25d14e554a9a9203852fbf3431f86
Austin <acreed1998@gmail.com>
2019-02-18 13:09:08 -0600

    add comments for server

--- server/index.js
+++ server/index.js
@@ -6,18 +6,22 @@ const _ = require('lodash');
 
 const app = express();
 
+// Probably not needed //
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, '../index.html')));
-// app.use(express.static('../index.html'));
+
+// Needed for React at Some Point // 
+// app.use(express.static(path.join(__dirname, [REACT DIRECTORY])));
 
 app.get('/', (req, res) => {
   console.log(req);
   res.send("Done");
 })
 
+// Able to set port and still work //
 const port = process.env.PORT || 3000;
 
+// Listen and console log current port //
 app.listen(port, () => {
   console.log(`listening on port ${port}!`);
 });
