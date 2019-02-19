12137b119b1dcedf4999a039278f7c5bf6ad80c5
Phuoc Nguyen <nguyen.phuoc23@gmail.com>
2019-02-19 14:17:55 -0600

    edited index component to render credientials, player, and recipe

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,16 +5,18 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Login from './components/login/login.jsx';
 import Recipe from './components/Recipe.jsx';
-import sampledata from './example_rfn_data';
+import sampleData from './example_rfn_data';
 import Credentials from './components/login/Credentials.jsx';
 import RecipeInstructions from './components/login/RecipeInstructions.jsx';
 import VideoPlayer from './components/login/VideoPlayer.jsx';
+import randomRecipe from './example_mealDB.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       recipes: [],
+      recipeOfTheDay: randomRecipe.meal// recipe of the day video
     };
   }
 
@@ -42,7 +44,12 @@ class App extends React.Component {
         </div>
 
         <div className="main">
-        
+        <div className="videoPlayer">
+          <VideoPlayer video={this.state.recipeOfTheDay} />
+        </div>
+        <div className="recipeInstructions">
+        <RecipeInstructions data={this.state.recipeOfTheDay}/>
+        </div>
         </div>
       </div>
     );
