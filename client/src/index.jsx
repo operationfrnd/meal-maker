c4d9c1835427e798d4dba61382e1f54662dd2613
Phuoc Nguyen <nguyen.phuoc23@gmail.com>
2019-02-20 16:06:04 -0600

    fixed eslint in index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,14 +3,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import Login from './components/login/login.jsx';
+import Login from './components/login/Login.jsx';
 import Recipe from './components/Recipe.jsx';
 import sampleData from './example_rfn_data';
 import Credentials from './components/login/Credentials.jsx';
 import RecipeInstructions from './components/login/RecipeInstructions.jsx';
 import VideoPlayer from './components/VideoPlayer.jsx';
 import randomRecipe from '../example_random.js';
-import Main from './components/main/Main.jsx'
+import Main from './components/main/Main.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -18,32 +18,33 @@ class App extends React.Component {
     this.state = {
       recipes: [],
       recipeOfTheDay: randomRecipe, // recipe of the day video
-      savedRecipes: []
+      savedRecipes: [],
     };
     this.getRandomRecipe = this.getRandomRecipe.bind(this);
     this.getRecipes = this.getRecipes.bind(this);
     this.getSavedRecipes = this.getSavedRecipes.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getRandomRecipe();
     // this.getSavedRecipes();
   }
 
-  // function to retrieve recipes to display 
+  // function to retrieve recipes to display
   getRecipes() {
     return axios.get('/food', {
       params: {
-        ingredients: ''
-      }}) // sends a GET request to serve at endpoint '/food'
+        ingredients: '',
+      },
+    }) // sends a GET request to serve at endpoint '/food'
       .then((results) => {
         // console.log(results);
         this.setState({ // change the state
-          recipes: results, // by making the data received back fron the server available 
-        })
+          recipes: results, // by making the data received back fron the server available
+        });
       }).catch((err) => {
-        console.log(err, 'error while retrieving data from server')
-      })
+        console.log(err, 'error while retrieving data from server');
+      });
   }
 
   getRandomRecipe() {
@@ -53,8 +54,8 @@ class App extends React.Component {
         // console.log(recipe, 'recipe');
         // console.log(this);
         this.setState({
-          
-          recipeOfTheDay: recipe.data
+
+          recipeOfTheDay: recipe.data,
         });
       })
       .catch((err) => {
@@ -66,7 +67,7 @@ class App extends React.Component {
     return axios.get('/saverecipes') // sends get request to server for saved recipes
       .then((recipes) => {
         this.setState({
-          savedRecipes: recipes
+          savedRecipes: recipes,
         });
       })
       .catch((err) => {
@@ -74,46 +75,44 @@ class App extends React.Component {
       });
   }
 
-  // 
+  //
 
   render() {
     console.log(this);
-
+    const { recipeOfTheDay, savedRecipes, recipes } = this.state;
     return (
       <div>
         <div>
-          <Login recipe={this.state.recipeOfTheDay}></Login>
+          <Login recipe={recipeOfTheDay} />
         </div>
         <div>
-          <Main recipes={this.state.recipes}
-                recipe={this.state.recipeOfTheDay}
-                savedRecipes={this.state.savedRecipes}>
-          </Main>
+          <Main
+            recipes={recipes}
+            recipe={recipeOfTheDay}
+            savedRecipes={savedRecipes}
+          />
         </div>
       </div>
-    )
+    );
 
 
+    // <div>
+    //   <div className="nav">
+    //     <span className="logo">Meal Maker</span>
+    //     <Credentials />
+    //   </div>
 
-
-      // <div>
-      //   <div className="nav">
-      //     <span className="logo">Meal Maker</span>
-      //     <Credentials /> 
-      //   </div>
-
-      //   <div className="main">
-      //   <div className="videoPlayer">
-      //     <VideoPlayer recipe={this.state.recipeOfTheDay} />
-      //   </div>
-      //   <div className="recipeInstructions">
-      //   <RecipeInstructions recipe={this.state.recipeOfTheDay}/>
-      //   </div>
-      //   </div>
-      // </div>
+    //   <div className="main">
+    //   <div className="videoPlayer">
+    //     <VideoPlayer recipe={this.state.recipeOfTheDay} />
+    //   </div>
+    //   <div className="recipeInstructions">
+    //   <RecipeInstructions recipe={this.state.recipeOfTheDay}/>
+    //   </div>
+    //   </div>
+    // </div>
     // );
   }
-
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
