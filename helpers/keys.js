9d3bb086f88e7a8ac9030b9924db2d8bda549f3e
Austin <acreed1998@gmail.com>
2019-02-20 11:00:38 -0600

    remove previous auth

--- helpers/keys.js
+++
@@ -1,5 +0,0 @@
-const recipieFoodNutritionApiKey = '2ec86674c2msh2b69061509e314bp1a1e51jsn9c448f4a87ea';
-const youTubeKey = 'AIzaSyBqt-V01lbpQSef577Rtjruk4EBGyeDWFY';
-
-module.exports.apiKey1 = recipieFoodNutritionApiKey;
-module.exports.apiKey2 = youTubeKey;
