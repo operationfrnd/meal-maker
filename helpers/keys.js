11775455c69ad2c8a225e31306b0e34b730040f9
pnguyen89 <nguyen.phuoc23@gmail.com>
2019-02-20 11:41:29 -0600

    Delete keys.js
    Do not need keys.js anymore.

--- helpers/keys.js
+++
@@ -1,6 +0,0 @@
-// API KEYS
-
-const apiKey1 = '0fa2a5162fmsh49f9d381bc89352p156ae0jsn2dbfb7ca959e' // Recipe Food Nutrition
-const apiKey2 = 'AIzaSyBqt - V01lbpQSef577Rtjruk4EBGyeDWFY' // Youtube
-
-module.exports = { apiKey1, apiKey2}
