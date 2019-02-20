9eff3840567c2fccc728a1e38bd17d712212a213
Phuoc Nguyen <nguyen.phuoc23@gmail.com>
2019-02-20 16:33:01 -0600

    fixed eslint in recipeInstructions.jsx

--- client/src/components/login/RecipeInstructions.jsx
+++ client/src/components/login/RecipeInstructions.jsx
@@ -1,13 +1,17 @@
 // component with a list of instructions for the recipe of the day which can be scrolled
 import React from 'react';
 
-var RecipeInstructions = ({ recipe }) => (
+const RecipeInstructions = ({ recipe }) => (
   <div className="instructions-list">
-    <b>Cook Time:</b> {recipe.cookTime} <br />
-    <b>Ingredients:</b> {recipe.ingredients} <br />
-    <b>Instructions:</b> {recipe.instructions} <br />
+    <h3>{recipe.name} Name</h3>
+    <b>Cook Time:</b>
+    {' '}
+    {recipe.cooktime}
+    <br />
+    <b>Ingredients:</b> {recipe.ingredients} 
+    <br />
+    <b>Instructions:</b> {recipe.instructions} 
+    <br />
   </div >
 );
-
-
 export default RecipeInstructions;
