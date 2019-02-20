96eccfb2799e0236e3cc606a3b8ffc83c38994b5
Phuoc Nguyen <nguyen.phuoc23@gmail.com>
2019-02-20 12:49:20 -0600

    created frame for recipeList and recipeListItem

--- client/src/components/main/RecipeList.jsx
+++ client/src/components/main/RecipeList.jsx
@@ -1,2 +1,16 @@
 // a component with 
 // a list of recipe list items spaced
+import React from 'react';
+
+var RecipeList = ({ recipes }) => {
+  const onClick = () => {
+    console.log('clicked');
+  }
+  return (
+  <div className="recipe-list">
+    {recipes.map((recipe) => <RecipeListItem recipe={recipe} key={recipe.recipeId} onClick={onClick.bind(this)} />)}
+  </div >
+)};
+
+
+export default RecipeList;
--- client/src/components/main/RecipeListItem.jsx
+++ client/src/components/main/RecipeListItem.jsx
@@ -4,3 +4,18 @@
 // a 'save/like recipe' button
 // a 'dislike recipe' button
 // ingredients 
+
+
+
+import React from 'react';
+
+var RecipeInstructions = ({ recipe }) => (
+  <div className="instructions-list">
+    <b>Recipe Name:</b> {recipe.name} <br />
+    <b>Cook Time:</b> {recipe.cookTime} <br />
+    <b>Uses:</b> {recipe.ingredients.usedIngredients} <br />
+  </div >
+);
+
+
+export default RecipeInstructions;
