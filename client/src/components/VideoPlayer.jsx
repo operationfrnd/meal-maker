7d6389ea1c7a70e041a851d7e82e1976deef6751
Phuoc Nguyen <nguyen.phuoc23@gmail.com>
2019-02-20 16:10:22 -0600

    solved eslint for videoplayer

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,20 +1,20 @@
 // component with a recipe name & a video player
 import React from 'react';
 
-var VideoPlayer = ({ recipe }) => (
+const VideoPlayer = ({ recipe }) => (
   <div className="video-player">
     <div className="video-player-details">
-      {`recipe.link`}
+      {'recipe.link'}
       <h3>{recipe.name}</h3>
     </div>
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${recipe.link}`} allowFullScreen></iframe>
+      <iframe title={recipe.name} className="embed-responsive-item" src={`https://www.youtube.com/embed/${recipe.link}`} allowFullScreen />
     </div>
   </div>
 );
 
 VideoPlayer.propTypes = {
-  recipe: React.PropTypes.object.isRequired
+  recipe: React.PropTypes.object.isRequired,
 };
 
 export default VideoPlayer;
