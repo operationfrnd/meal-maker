3e6c38568ac1bece07828206545d2927bb63ec1b
Phuoc Nguyen <nguyen.phuoc23@gmail.com>
2019-02-21 13:06:25 -0600

    created AutoComplete component, added to Search Component, able to autocomplete and click item from hardcoded ingredients

---
+++ client/src/components/main/AutoComplete.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+class AutoComplete extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ingredients = [
+      'apple',
+      'beef',
+      'banana',
+      'chicken',
+      'sugar',
+      'eggs',
+    ];
+    this.state = {
+      suggestions: [],
+      text: '',
+    };
+    this.onTextChange = this.onTextChange.bind(this);
+    this.suggestionSelected = this.suggestionSelected.bind(this);
+  }
+
+  onTextChange(e) {
+    const { value } = e.target;
+    let suggestions = [];
+    if (value.length > 0) {
+      const regex = new RegExp(`^${value}`, 'i');
+      suggestions = this.ingredients.sort().filter(v => regex.test(v));
+    }
+    this.setState({ suggestions, text: value });
+  }
+
+  suggestionSelected(value) {
+    this.setState({
+      text: value,
+      suggestions: [],
+    });
+  }
+
+  renderSuggestions() {
+    const { suggestions } = this.state;
+    if (suggestions.length === 0) {
+      return null;
+    }
+    return (
+      <ul>
+        {suggestions.map(ingredient => <li onClick={() => this.suggestionSelected(ingredient)} key={ingredient}>{ingredient}</li>)}
+      </ul>
+    );
+  }
+
+  render() {
+    const { text } = this.state;
+    return (
+      <div>
+        <input value={text} onChange={this.onTextChange} type="text" />
+        {this.renderSuggestions()}
+      </div>
+    );
+  }
+}
+
+export default AutoComplete;
--- client/src/components/main/Search.jsx
+++ client/src/components/main/Search.jsx
@@ -1,12 +1,24 @@
 // component with an input form, a + button and a submit button
 
-import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
+import React, { Component, Fragment } from 'react';
+// import PropTypes from "prop-types";
+import AutoComplete from './AutoComplete.jsx';
 
-const Search = () => {
-  return (
-    <h2>Search</h2>
-  );
-};
+// eslint-disable-next-line react/prefer-stateless-function
+class Search extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div>
+        <h2>Search</h2>
+        <AutoComplete />
+        <h3>more stuff</h3>
+      </div>
+    );
+  }
+}
 
 export default Search;
