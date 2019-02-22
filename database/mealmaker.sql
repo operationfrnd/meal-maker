83db386f75cddb04666636d479f03ebc905edad2
Austin <acreed1998@gmail.com>
2019-02-22 11:36:52 -0600

    add recipeImageLink to recipeofhedaytable

--- database/mealmaker.sql
+++ database/mealmaker.sql
@@ -55,6 +55,7 @@ CREATE TABLE RecipeOfTheDay (
   link VARCHAR(255) NOT NULL,
   instructions TEXT NOT NULL,
   idRecipe INTEGER NOT NULL,
+  recipeImageLink VARCHAR(255) NOT NULL,
   cooktime INTEGER NOT NULL,
   date INTEGER NOT NULL,
   PRIMARY KEY (id)
--- helpers/dbHelpers.js
+++ helpers/dbHelpers.js
@@ -80,9 +80,9 @@ const selectAllRecipeOfTheDay = (callback) => {
   })
 };
 
-const saveRecipeOfTheDay = (revcipeName, videoLink, recipeInstructions, ourDbRecipeId, cooktime, currentDate) => {
-  let q = [revcipeName, videoLink, recipeInstructions, ourDbRecipeId, cooktime, currentDate];
-  connection.query('INSERT INTO RecipeOfTheDay (name, link, instructions, idRecipe, cooktime, date) VALUES (?, ?, ?, ?, ?, ?)', q, (err, results) => {
+const saveRecipeOfTheDay = (revcipeName, videoLink, recipeInstructions, ourDbRecipeId, cooktime, recipeImageLink, currentDate) => {
+  let q = [revcipeName, videoLink, recipeInstructions, ourDbRecipeId, cooktime, recipeImageLink, currentDate];
+  connection.query('INSERT INTO RecipeOfTheDay (name, link, instructions, idRecipe, cooktime, recipeImageLink, date) VALUES (?, ?, ?, ?, ?, ?, ?)', q, (err, results) => {
     if (err) {
       console.log('could not save recipe of the day to database');
     } else {
