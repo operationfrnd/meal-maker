5d7703db0b0c40de3949ad2a1b5669308d0ee3cd
Julien de la Mettrie <jdelamettrie@gmail.com>
2019-02-25 19:37:48 -0600

    vidPlayer component cleaned up

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -4,54 +4,29 @@ import React from 'react';
 class VideoPlayer extends React.Component {
   constructor(props) {
     super(props);
-    // console.log(props);
+    const { recipe } = this.props;
     this.state = {
-      currentVideo: this.props.recipe,
+      currentVideo: recipe,
     };
   }
 
-  // componentDidMount() {
-  //   this.setState({
-  //     currentVideo: this.props.recipe,
-  //   })
-  // }
-
   render() {
-    // console.log(this);
     const { currentVideo } = this.state;
-
     return (
       <div className="video-player">
-        {/* <div className="video-player-details">
-      <h3>{recipe.name}</h3>
-    </div> */}
-        {/* <div className="embed-responsive embed-responsive-16by9">
-      <iframe title={recipe.name} className="embed-responsive-item" src={`https://www.youtube.com/embed/${recipe.link}`} allowFullScreen />
-    </div> */}
         <div className="video-container">
-          <iframe width="560" height="349" src={`https://www.youtube.com/embed/${currentVideo.link}`} frameBorder="0" allowFullScreen></iframe>
+          <iframe
+            title={currentVideo.link}
+            width="560"
+            height="349"
+            src={`https://www.youtube.com/embed/${currentVideo.link}`}
+            frameBorder="0"
+            allowFullScreen
+          />
         </div>
       </div>
     );
   }
 }
-
-// const VideoPlayer = ({ recipe, recipeOfTheDay }) => (
-//   <div className="video-player">
-//     {/* <div className="video-player-details">
-//       <h3>{recipe.name}</h3>
-//     </div> */}
-//     {/* <div className="embed-responsive embed-responsive-16by9">
-//       <iframe title={recipe.name} className="embed-responsive-item" src={`https://www.youtube.com/embed/${recipe.link}`} allowFullScreen />
-//     </div> */}
-//     <div className="video-container">
-//       <iframe width="560" height="349" src={`https://www.youtube.com/embed/${recipe.link}`} frameBorder="0" allowFullScreen></iframe>
-//     </div>
-//   </div>
-// );
-
-// VideoPlayer.propTypes = {
-//   recipe: React.PropTypes.object.isRequired,
-// };
 
 export default VideoPlayer;
